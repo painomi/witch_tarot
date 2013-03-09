68f50951b97996282be6f2639c6156997fb02c7b
painomi ringo <painomi.ringo@gmail.com>
2013-03-10 08:33:58 +0900

    テストがうまく書けない。。。

--- witch_tarot_spec.rb
+++ witch_tarot_spec.rb
@@ -6,15 +6,15 @@ describe WitchTarot, "ウィッチ・タロー" do
 	describe 'Witchtarot::name' do
 		context '引数なしの場合' do
 			it '1D6+1文字の名前を生成' do
-				srand(0)
-				WitchTarot::name.should== 'MIIKRI'
+				WitchTarot.should_receive(:rand).exactly(3).times.and_return(1)
+				WitchTarot::name.should== 'SR'
 			end
 		end
 		
 		context '文字数指定の場合' do
 			it '指定文字数の名前を生成' do
-				srand(0)
-				WitchTarot::name(7).should== 'DELCISW'
+				WitchTarot.should_receive(:rand).exactly(7).times.and_return(0)
+				WitchTarot::name(7).should== 'TTIFISO'
 			end
 		end
 	end
