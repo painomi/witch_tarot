1d5734715cc61baa5f73bce37a77369f2d15097a
painomi ringo <painomi.ringo@gmail.com>
2013-06-09 08:47:12 +0900

    srand使ってテストを書きなおした

--- witch_tarot_spec.rb
+++ witch_tarot_spec.rb
@@ -4,17 +4,19 @@ require './witch_tarot.rb'
 
 describe WitchTarot, "ウィッチ・タロー" do
 	describe 'Witchtarot::name' do
+		before do
+			srand(0)
+		end
+		
 		context '引数なしの場合' do
 			it '1D6+1文字の名前を生成' do
-				WitchTarot.should_receive(:rand).exactly(3).times.and_return(1)
-				WitchTarot::name.should== 'SR'
+				WitchTarot::name.should== 'MIIKRI'
 			end
 		end
 		
 		context '文字数指定の場合' do
 			it '指定文字数の名前を生成' do
-				WitchTarot.should_receive(:rand).exactly(7).times.and_return(0)
-				WitchTarot::name(7).should== 'TTIFISO'
+				WitchTarot::name(7).should== 'SEAUCCO'
 			end
 		end
 	end
